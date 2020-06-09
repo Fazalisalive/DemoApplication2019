6cd0e9049706d47b371358b3f0e832bc519d9ded
Fazal Ur Rehman Azad <fazal8ciit@gmail.com>
2020-06-09 20:42:45 +0500

    Update Dinner.js

--- src/Dinner.js
+++ src/Dinner.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Dinner(props){
     return(
-    <div><h1>Today we are serving {props.dishName} and {props.sweetDish} in dinner.</h1></div>
+    <div>
+        <h1>Today we are serving {props.dishName} and {props.sweetDish} in dinner.</h1>
+    </div>
     )
 }
 export default Dinner
